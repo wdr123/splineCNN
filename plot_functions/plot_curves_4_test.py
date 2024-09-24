deaa97d8acbd84b42f0c103355f1f83c8cf539d3
Dingrong_Wang <dw7445@rit.edu>
2024-09-24 11:40:06 -0400

    plot_function

--- plot_functions/plot_curves_4_test.py
+++ plot_functions/plot_curves_4_test.py
@@ -35,7 +35,7 @@ time_step = [3,4,5,6,7]
 # title = "LSTM-maze3D Input Length Ablation (40,50,60)"
 # title = "Test Classification Accuracy for different RL time step (3,4,5,6,7)"
 
-title = "Test TRE Experiment"
+title = "Training Cross Entropy Loss"
 
 # font = {'family' : 'normal',
 #         'weight' : 'bold',
@@ -56,13 +56,13 @@ fig = plt.figure(figsize=(10,5))
 ax = fig.add_subplot(111)
 
 # set labels and font size
-ax.set_xlabel("Epoch(*10)", fontsize = 15)
-ax.set_ylabel("Average Test TRE", fontsize = 15)
+ax.set_xlabel("Epoch", fontsize = 15)
+ax.set_ylabel("Training CE Loss", fontsize = 15)
 ax.tick_params(axis='x', labelsize=15)
 ax.tick_params(axis='y', labelsize=15)
 
 
-for idx, data_path in enumerate([data_path_attn, data_path_30, data_path_128, data_path_256, data_path_512]):
+for idx, data_path in enumerate([data_path_attn,]):
 # for idx, data_path in enumerate([data_path_attn, data_path_woattn]):
 # for idx, data_path in enumerate([data_path_40, data_path_50]):
 # for idx, data_path in enumerate([ts_path_30, ts_path_40]):
@@ -90,30 +90,30 @@ for idx, data_path in enumerate([data_path_attn, data_path_30, data_path_128, da
         
         with sns.axes_style("darkgrid"):
 
-            if idx == 0:
-                plot_this = plot_this + 2.395
-                ax.plot(plot_this, label=f"B1", c=clrs[idx])
-                ax.fill_between(np.arange(len(plot_this)), plot_this-error, plot_this+error, alpha=0.3, facecolor=clrs[idx])
-            elif idx == 1:
-                plot_this = plot_this + 2.323
-                ax.plot(plot_this, label=f"B2", c=clrs[idx])
-                ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
-                        facecolor=clrs[idx])
-            elif idx == 2:
-                plot_this = plot_this + 2.238
-                ax.plot(plot_this, label=f"B3", c=clrs[idx])
-                ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
-                        facecolor=clrs[idx])
-            elif idx == 3:
-                plot_this = plot_this + 2.245
-                ax.plot(plot_this, label=f"B4", c=clrs[idx])
-                ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
-                        facecolor=clrs[idx])
-            elif idx == 4:
-                plot_this = plot_this + 2.110
-                ax.plot(plot_this, label=f"Ours", c=clrs[idx])
-                ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
-                        facecolor=clrs[idx])
+            # if idx == 0:
+            #     plot_this = plot_this + 2.395
+            #     ax.plot(plot_this, label=f"B1", c=clrs[idx])
+            #     ax.fill_between(np.arange(len(plot_this)), plot_this-error, plot_this+error, alpha=0.3, facecolor=clrs[idx])
+            # elif idx == 1:
+            #     plot_this = plot_this + 2.323
+            #     ax.plot(plot_this, label=f"B2", c=clrs[idx])
+            #     ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
+            #             facecolor=clrs[idx])
+            # elif idx == 2:
+            #     plot_this = plot_this + 2.238
+            #     ax.plot(plot_this, label=f"B3", c=clrs[idx])
+            #     ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
+            #             facecolor=clrs[idx])
+            # elif idx == 3:
+            #     plot_this = plot_this + 2.245
+            #     ax.plot(plot_this, label=f"B4", c=clrs[idx])
+            #     ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
+            #             facecolor=clrs[idx])
+            # elif idx == 4:
+            plot_this = plot_this / 20
+            ax.plot(plot_this, label=f"Ours", c=clrs[idx])
+            ax.fill_between(np.arange(len(plot_this)), plot_this - error, plot_this + error, alpha=0.3,
+                    facecolor=clrs[idx])
             # else:
             #     ax.plot(plot_this , label=f"latent_dimension_{latent_dim[idx]}", c=clrs[idx])
             #     ax.fill_between(np.arange(200), plot_this- error, plot_this + error, alpha=0.3,
